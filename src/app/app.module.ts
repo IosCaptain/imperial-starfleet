75e6d1e487a41f80d68a84178d9ae0f2ec1ef08d
Abel <adavid9988@gmail.com>
2022-01-12 21:27:16 -0800

    Step 1 and 2 Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DeathSquareComponent } from './death-square/death-square.component';
+import { TurretComponent } from './turret/turret.component';
+import { LaserCannonComponent } from './laser-cannon/laser-cannon.component';
+import { TieFighterComponent } from './tie-fighter/tie-fighter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeathSquareComponent,
+    TurretComponent,
+    LaserCannonComponent,
+    TieFighterComponent
   ],
   imports: [
     BrowserModule
